3d9cbd35bb3462c3b05de86147c9f6f962fead1a
arthi <arthi.vithiananthan@gmail.com>
2024-07-25 15:50:58 -0400

    creating frames in protect

--- tools/fw_protect.py
+++ tools/fw_protect.py
@@ -5,65 +5,68 @@
 
 """
 Firmware Bundle-and-Protect Tool
-
 """
 import argparse
 from pwn import *
-
+from util import *
 
 def protect_firmware(infile, outfile, version, message):
-# FRAME 0: METADATA
-    # packing metadata
-    metadata = p16(version, endian='little') + p16(len(firmware), endian='little')  
-    
-    # put metadata into a frame with 'METADATA'
-    
-    version_frame = metadata + message.encode()
-    
-    #hash it (place holder for now )
-    version_hash = b'\x00\x00'
-    
-    #append hash to frame
-    version_frame = version_frame + version_hash
-
-    # write version frame to outfile (writing this first)
-    with open(outfile, "wb+") as outfile:
-        outfile.write(version_frame)
-
-# FRAME 1: FIRMWARE DATA
+    # FRAME 0: METADATA
     # Load firmware binary from infile
     with open(infile, "rb") as fp:
         raw_firmware = fp.read()
+    firmware = raw_firmware
 
-    # encryption of the firmware here
-    firmware = raw_firmware #for now just keeping this as the "encryption step"
-    
-    #split the now encrypted firmware into frames
-    for i in range(0,len(firmware)-1, 20):
-        # split the firmware into chunks of (20 bytes?)
-        chunk = firmware[i:i+20] 
-        #^^^^this definitely isnt how this should be done, need to double check
-        
+    # Packing metadata
+    metadata = p16(version, endian='little') + p16(len(firmware), endian='little')
+    version_frame = metadata + message.encode()
+
+    # Hash it (placeholder for now)
+    version_hash = b'\x00\x00'
+
+    # Append hash to frame
+    version_frame = version_frame + version_hash
+
+    # Write version frame to outfile (writing this first)
+    with open(outfile, "wb+") as out_fp:
+        out_fp.write(version_frame)
+
+    # FRAME 1: FIRMWARE DATA
+    # Encryption of the firmware here
+    # For now just keeping this as the "encryption step"
+    iv = b'6576'
+
+    # Split the now encrypted firmware into frames
+    for i in range(0, len(firmware), 20):
+        # Split the firmware into chunks of 20 bytes
+        chunk = firmware[i:i + 20]
+        print_hex(chunk)
+
         frame_size = p16(len(chunk), endian='little')
-        # hash it
+        # Hash it
         data_hash = b'\x01\x01'
 
-        # add message & hash to firmware frame
-        data_frame = frame_size + chunk + data_hash
+        # Add message & hash to firmware frame
+        data_frame = iv + frame_size + chunk + data_hash
+        print_hex(data_frame)
 
-        #write frames into outfile
-        with open(outfile, "wb+") as outfile:
-            outfile.write(data_frame)
-    
-#FRAME 2: END
+        # Write frames into outfile
+        with open(outfile, "ab+") as out_fp:
+            out_fp.write(data_frame)
+
+    # FRAME 2: END
     end_message = b'END'
     end_hash = b'\x02\x02'
-    end_size = p16(len(end_hash), endian = 'little')
-    end_frame = end_message + end_size+ end_hash
-    
-    with open(outfile, "wb+") as outfile:
-        outfile.write(end_frame)
+    end_size = p16(len(end_hash), endian='little')
+    end_frame = end_message + end_size + end_hash
 
+    # Append end frame to outfile
+    with open(outfile, "ab+") as out_fp:
+        out_fp.write(end_frame)
+
+    # Read and print the final output file
+    with open(outfile, "rb") as out_fp:
+        print_hex(out_fp.read().hex())
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Firmware Update Tool")
@@ -71,8 +74,7 @@ if __name__ == "__main__":
     parser.add_argument("--outfile", help="Filename for the output firmware.", required=True)
     parser.add_argument("--version", help="Version number of this firmware.", required=True)
     parser.add_argument("--message", help="Release message for this firmware.", required=True)
-    # need to add an argument of the key for encryption --> idk how to do this without exposing 
-    #the key but whatever
+
     args = parser.parse_args()
 
     protect_firmware(infile=args.infile, outfile=args.outfile, version=int(args.version), message=args.message)
