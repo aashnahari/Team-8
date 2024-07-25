533d6074d0f5dbeeffc1dd5a2b81d39f0c32359d
Aashna <aashna_hari@horacemann.org>
2024-07-25 09:45:02 -0400

    comments and documentation

--- tools/bl_build.py
+++ tools/bl_build.py
@@ -1,6 +1,4 @@
 /usr/bin/env python
-
-# hey, it pushed!
 # Copyright 2024 The MITRE Corporation. ALL RIGHTS RESERVED
 # Approved for public release. Distribution unlimited 23-02181-25.
 
@@ -27,6 +25,12 @@ BOOTLOADER_DIR = os.path.join(REPO_ROOT, "bootloader")
 ROOT_KEY = b"hi"
 
 def create_header_file(aes, hmac, rsaPriv):
+    """
+    Creates header file to send to bootloader.c so it can decrypt the aes key for the firmware decryption.
+
+    INPUT: encrypted AES key, HMAC key, RSA private key
+    OUTPUT: N/A
+    """
     with open('../Team-8/tools/header.h', 'w') as header_file:
         header_file.write("#ifndef HEADER_H\n")
         header_file.write("#define HEADER_H\n\n")
@@ -37,10 +41,25 @@ def create_header_file(aes, hmac, rsaPriv):
 
 
 def delete_header_file(file_path):
+    """
+    Deletes header file so hackers can't see the keys!
+
+    INPUT: header file path
+    OUTPUT: N/A
+    """
     if os.path.exists(file_path):
         os.remove(file_path)
 
 def key_derivation(root_key):
+    """
+    Derives AES and HMAC keys using the HKDF key derivation function and a root key. 
+    Uses a salt for randomization.
+    Encrypts AES key using RSA.
+    Writes all keys to secrets file.
+
+    INPUT: root key
+    OUTPUTS: encrypted AES key, HMAC key, RSA private key
+    """
     salt = os.urandom(16)
     info1 = b"AES key"
     info2 = b"HMAC key"
@@ -48,25 +67,29 @@ def key_derivation(root_key):
 
     ikm = root_key
 
+    #----------AES KEY----------
     hkdf = HKDF(
     algorithm=hashes.SHA256(),
-    length=len(ROOT_KEY),  # Length of the keying material in bytes
+    length=len(ROOT_KEY),  
     salt=salt,
     info=info1,
     backend=backend)
-    aes_key = hkdf.derive(ikm)
 
+    aes_key = hkdf.derive(ikm)
+    
+    #----------HMAC KEY----------
     hkdf = HKDF(
     algorithm=hashes.SHA256(),
-    length=len(ROOT_KEY),  # Length of the keying material in bytes
+    length=len(ROOT_KEY), 
     salt=salt,
     info=info2,
     backend=backend)
     
-
     hmac_key = hkdf.derive(ikm)
+
+    #----------WRITE TO SECRETS FILE----------
     with open('../Team-8/tools/secret_build_output.txt', 'wb') as file:
-        key_arr = generate_and_encrypt(aes_key) # encrypts AES key with generated RSA key
+        key_arr = generate_and_encrypt(aes_key) # encrypts AES key with generated RSA public key
         rsa_private = key_arr[0][1]
         rsa_public = key_arr[0][0]
         aes_key_enc = key_arr[1]
@@ -78,20 +101,18 @@ def key_derivation(root_key):
         file.write(b"\n")
         file.write(rsa_public)
         file.write(b"\n")
-    # with open('../Team-8/tools/secret_build_output.txt', 'rb') as file:
-    #     print(file.read().hex())
        
 
 
     return aes_key_enc, hmac_key, rsa_private
 
-
-
-
-
-
 def make_bootloader() -> bool:
-    # Build the bootloader from source.
+    """
+    Builds the bootloader from source.
+
+    INPUT: N/A
+    OUTPUT: True if bootloader is made, False if not
+    """
 
     os.chdir(BOOTLOADER_DIR)
 
@@ -101,19 +122,22 @@ def make_bootloader() -> bool:
     # Return True if make returned 0, otherwise return False.
     return status == 0
 
-# input: unencrypted AES key
-# output: RSA key pair and encrypted AES key
-# output format: [[public key, private key], encrypted AES key]
 def generate_and_encrypt(unenc_key):
+    """
+    Generates RSA public and private keys for AES key encryption.
+    Encrypts AES key using RSA public key.
+
+    INPUT: AES key
+    OUTPUT: [[RSA public key, RSA private key], encrypted AES key]
+    """
     rsa_private = RSA.generate(2048)
     rsa_public = rsa_private.public_key()
     enc_aes = rsa_public.encrypt(unenc_key)
     return [[rsa_public, rsa_private], enc_aes]
 
 if __name__ == "__main__":
-    make_bootloader()
 
     aes_enc, hmac, rsaPriv = key_derivation(ROOT_KEY)
     create_header_file(aes_enc, hmac, rsaPriv)
     delete_header_file("../Team-8/tools/header.h")
-    #make_bootloader()
+    make_bootloader()
